aa6c6c256baa77ef046533b6ce08638fb0faae0f
Developer-Bunny <63245704+Developer-Bunny@users.noreply.github.com>
2020-04-06 18:10:06 +0200

    Tipos de contadores de powershell

---
+++ Powershell/Contadores.ps1
@@ -0,0 +1,21 @@
+﻿#Primero con bucle Do/While
+Write-host "-----";
+$token = 0;
+Do {
+    Write-Host $token;
+    $token = $token + 3;
+} While ($token -lt 100);
+
+#Segundo Programa bucle Do/Until
+Write-host "-----";
+$token = 0;
+Do {
+    Write-Host $token;
+    $token = $token + 3;
+} Until ($token -ge 100);
+
+#Tercer programa bucle for
+Write-host "-----";
+For ($token = 0;$token -lt 100;$token = $token + 3) {
+Write-Host $token;
+}
